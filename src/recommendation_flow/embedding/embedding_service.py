73059feaa4952bf0cf055c9115aa7c18ce575554
MinhTrideptrai <ngminhtri21102004@gmail.com>
2025-12-09 16:18:17 +0000

    refactor(recommendation-flow): implement HTTP status code checking mechanism and optimize the embedding generation flow

--- src/recommendation_flow/embedding/embedding_service.py
+++ src/recommendation_flow/embedding/embedding_service.py
@@ -7,7 +7,7 @@ from typing import List
 from google import genai
 from google.genai import types
 from recommendation_flow.utils.helpers import parse_api_keys
-
+from recommendation_flow.utils.status_code import GoogleAPIChecker
 class EmbeddingService:
   def __init__(
     self,
@@ -22,8 +22,19 @@ class EmbeddingService:
 
     Args:
       api_key (str): API key for Google Generative AI.
-      model_name (str, optional): Embedding model name. Defaults to "gemini-embedding-001".
+      model_name (str, optional): Embedding model name. Defaults to "text-embedding-004".
       batch_size (int, optional): Number of documents per batch. Defaults to 200.
+      request_per_minute_limit (int, optional): Maximum number of requests allowed per minute. Defaults to 55.
+      request_per_day_limit (int, optional): Maximum number of requests allowed per day. Defaults to 1450.
+      
+    Raises:
+      ValueError: If no valid API keys are provided.
+
+    Notes:
+      - The service supports multiple API keys, which are parsed and rotated 
+        automatically to handle rate limits.
+      - The client is initialized upon instantiation to ensure readiness 
+        for embedding requests.
     """
     self.api_keys = parse_api_keys(api_key)
     if not self.api_keys:
@@ -34,26 +45,73 @@ class EmbeddingService:
     self.batch_size = batch_size
     self.rpm_limit = request_per_minute_limit
     self.rpd_limit = request_per_day_limit
-    self._initialize_client()
     
+    self._initialize_client()
+  
+  # -----------------------------------------------------------------------------------
   def _initialize_client(self):
     current_key = self.api_keys[self.current_key_idx]
-    masked_key = f"...{current_key[-4:]}" if len(current_key) > 4 else "INVALID"
-    print(f"Initializing GenAI Client with key index {self.current_key_idx} ({masked_key})")
+    masked_key = f"...{current_key[-4:]}"
+    print(f"Initializing GenAI Client with key[{self.current_key_idx}] ({masked_key})")
     
     self.client = genai.Client(api_key=current_key)
-    
+  
+  # -----------------------------------------------------------------------------------
   def _rotate_key(self) -> bool:
     next_idx = self.current_key_idx + 1
-    if next_idx < len(self.api_keys):
-      print(f"Switching API Key from index {self.current_key_idx} to {next_idx} due to error.")
-      self.current_key_idx = next_idx
-      self._initialize_client()
-      return True
-    else:
-      print("All API keys have been exhausted!")
+    if next_idx >= len(self.api_keys):
+      print("All API keys exhausted - cannot rotate anymore.")
       return False
+    
+    print(f"Rotating key {self.current_key_idx} to {next_idx}")
+    self.current_key_idx = next_idx
+    self._initialize_client()
+    time.sleep(0.5)
+    return True
+  
+  # -----------------------------------------------------------------------------------
+  def _call_embed_api(self, contents, task_type):
+    """
+    Centralized API call with retry, error handling and key rotation.
+    """
+    max_attempts = len(self.api_keys) * 2
+    
+    for attempt in range(max_attempts):
+      try:
+        res = self.client.models.embed_content(
+          model=self.model_name,
+          contents=contents,
+          config=types.EmbedContentConfig(
+            task_type = task_type,
+            output_dimensionality=768
+          )
+        )
+      
+      except Exception as e:
+        status = GoogleAPIChecker.extract_staus_code(e)
+        print(f"[ERROR] key[{self.current_key_idx}] status={status} error={e}")
+        
+        # No status -> unknown error -> retry
+        if status is None:
+          print(f"Unknown Error. Retrying with backoff...")
+          time.sleep(1.5)
+          continue
+        
+        # 5xx -> server error -> retry
+        if status >= 500:
+          print(f"Server Error. Retrying after 2s...")
+          time.sleep(2)
+          
+        # Should rotate or not?
+        if GoogleAPIChecker.should_rotate_key(status):
+          if self._rotate_key():
+            continue
+          else:
+            raise RuntimeError(f"All API keys exhausted (status {status}).")
+          
+      raise RuntimeError(f"Client error {status}: {e}")
 
+  # -----------------------------------------------------------------------------------
   def embed_documents(self, texts: List[str]) -> List[List[float]]:
     """
     Generate embeddings for dataset documents (combined_text).
@@ -65,35 +123,30 @@ class EmbeddingService:
     Returns:
       np.ndarray: 2D array (num_docs, embedding_dim).
     """
-    if not texts: return []
-    contents = [{"parts": [{"text": t}]} for t in texts]
-    max_attempts = len(self.api_keys)
+    if not texts: 
+      return []
     
-    for attempt in range(max_attempts):
-      try:
-        res = self.client.models.embed_content(
-          model=self.model_name,
-          contents=contents,
-          config=types.EmbedContentConfig(
-            task_type="RETRIEVAL_DOCUMENT",
-            output_dimensionality=768
-          )
-        )
+    contents = [{"parts": [{"text": t}]} for t in texts]
+    response = self._call_embed_api(contents, "RETRIEVAL_DOCUMENT")
+    return [emb.values for emb in response.embeddings]
+  
+  # -----------------------------------------------------------------------------------
+  def embed_query(self, text: str) -> np.ndarray:
+    """
+    Generate an embedding for user input combined_text.
+    Task: RETRIEVAL_QUERY
 
-        vectors = [emb.values for emb in res.embeddings]
-        return vectors
-      except Exception as e:
-        print(f"Error with key index {self.current_key_idx}: {e}")
-        
-        if self._rotate_key():
-          time.sleep(1)
-          continue
-        else:
-          print("Fatal Error: Embedding failed with all available keys.")
-          return []
-        
-    return []
+    Args:
+      text (str): User input string.
 
+    Returns:
+      np.ndarray: 1D vector (embedding_dim,).
+    """
+    contents = [{"parts": [{"text": text}]}]
+    response = self._call_embed_api(contents, "RETRIEVAL_QUERY")
+    return np.array(response.embeddings[0].values)
+
+  # -----------------------------------------------------------------------------------
   def generate_in_batches(self, df: pd.DataFrame) -> pd.DataFrame:
     """
     Generate embeddings for a full DataFrame in batches and return
@@ -124,7 +177,7 @@ class EmbeddingService:
         elapsed_time = time.time() - start_time
         
         if elapsed_time < 60:
-          sleep_time = 60 - elapsed_time + 1
+          sleep_time = 60 - elapsed_time + 0.5
           print(f"Rate limit approaching ({min_request_count} reqs in {elapsed_time:.2f}s). Sleeping for {sleep_time:.2f}s...")
           time.sleep(sleep_time)
           
@@ -158,36 +211,3 @@ class EmbeddingService:
       return pd.DataFrame(columns=["doi", "combined_text", "vector"])
 
     return pd.concat(results, ignore_index=True)
-
-  def embed_query(self, text: str):
-    """
-    Generate an embedding for user input combined_text.
-    Task: RETRIEVAL_QUERY
-
-    Args:
-      text (str): User input string.
-
-    Returns:
-      np.ndarray: 1D vector (embedding_dim,).
-    """
-    contents = [{"parts": [{"text": text}]}]
-    max_attempts = len(self.api_keys)
-    
-    for attempt in range(max_attempts):
-      try:
-        res = self.client.models.embed_content(
-          model=self.model_name,
-          contents=contents,
-          config=types.EmbedContentConfig(
-            task_type="RETRIEVAL_QUERY",
-            output_dimensionality=768
-          )
-        )
-        return np.array(res.embeddings[0].values)
-      except Exception as e:
-        print(f"Query embedding error with key {self.current_key_idx}: {e}")
-        if self._rotate_key():
-          continue
-        else:
-          raise e
-    return np.array([])
