395c357955889b34d69be58086ab86703b501590
MinhTrideptrai <ngminhtri21102004@gmail.com>
2025-12-09 06:36:21 +0000

    feat(recommendation-flow): implement checking key mechanism to backup

--- src/runners/run_embedding.py
+++ src/runners/run_embedding.py
@@ -1,28 +1,45 @@
 import logging
 
+from integration.secrets import get_api_key
 from warehouse.core.session import postgres_session
 from warehouse.core.data_access import WarehouseDataAccess
 
+from recommendation_flow.preprocessing.data_processor import DataPreprocessor
+from recommendation_flow.preprocessing.data_combiner import DocumentCombiner
+from recommendation_flow.embedding.embedding_service import EmbeddingService
+
 def run_embedding() -> None:
-    with postgres_session() as pg_session:
-        warehouse_access = WarehouseDataAccess(pg_session)
+  # Configuration
+  preprocessor = DataPreprocessor()
+  combiner = DocumentCombiner()
+  embedding_service = EmbeddingService(
+    api_key=get_api_key(),
+    batch_size=100
+  )
 
-        """
-        Firstly, read data from warehouse
+  with postgres_session() as pg_session:
+    warehouse_access = WarehouseDataAccess(pg_session)
 
-        data = warehouse_access.read(table = 'paper_rcm_feature', conditions = 'vector IS NULL')
-        """
+    # 1. Reading data from warehouse
+    logging.info("Step 1: Reading data from warehouse...")
+    data = warehouse_access.read(
+      table='paper_rcm_feature',
+      conditions='vector IS NULL'
+    )
 
+    # 2. Preprocessing and Combining text
+    logging.info("Step 2: Preprocessing and Combining text...")
+    processed_df = preprocessor.transform(data)
+    processed_df['combined_text'] = processed_df.apply(combiner.combine_documents, axis=1)
 
-        """ Embedding pipeline here """
+    # 3. Embedding in batches and Updating to database
+    logging.info("Step 3: Embedding in batches and Updating...")
+    data_to_update = embedding_service.generate_in_batches(processed_df)
+    warehouse_access.update(
+      table='paper',
+      data=data_to_update,
+      key='doi',
+      update_cols=['vector', 'combined_text']
+    )
 
-
-        """
-        Lastly, update vector into warehouse
-
-        warehouse_access.update(table = 'paper',
-                                data = data_after_embedding,
-                                key = 'doi',
-                                update_cols = ['vector'])
-        """
-
+  logging.info("Embedding pipeline completed successfully.")
